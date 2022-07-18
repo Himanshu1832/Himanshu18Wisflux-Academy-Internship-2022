e4adfbed74b178f0d90a96c95e6f41d99a1c53f4
Himanshu Patni <96659387+Himanshu1832@users.noreply.github.com>
2022-07-18 18:50:45 +0530

    Add files via upload

---
+++ Day 11/index.js
@@ -0,0 +1,43 @@
+var form = document.getElementById("myform");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  var search = document.getElementById("search").value;
+
+  var original = search.split(" ").join("");
+
+  document.getElementById("result").innerHTML = "";
+
+  fetch("https://api.github.com/users/" + original)
+    .then((result) => result.json())
+    .then((data) => {
+      console.log(data);
+      fetch("https://api.github.com/users/" + original + "/events")
+        .then((result) => result.json())
+        .then((data) => {
+          var result = document.getElementById("result");
+          for (i = 0; i < 9; i++) {
+            let li = document.createElement("h1");
+            let a = document.createElement("a");
+            var q = "https://www.github.com/" + original;
+            a.href = q;
+            a.innerText = "click";
+            console.log(a);
+            li.innerText = data[i].repo.name;
+            result.appendChild(a);
+            result.appendChild(li);
+          }
+        });
+    });
+});
+
+let sum = 0;
+const numbers = [65, 44, 12, 4];
+numbers.forEach(myFunction);
+
+function myFunction(item) {
+  sum += item;
+}
+
+// HimanshuPatni
