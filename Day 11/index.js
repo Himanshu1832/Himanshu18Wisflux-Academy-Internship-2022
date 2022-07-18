1d909fe7fbc4b65a7f0fa27525c9fc9bdfc1a1f1
Himanshu Patni <96659387+Himanshu1832@users.noreply.github.com>
2022-07-18 18:54:35 +0530

    Update index.js

--- Day 11/index.js
+++ Day 11/index.js
@@ -32,12 +32,5 @@ form.addEventListener("submit", function (e) {
     });
 });
 
-let sum = 0;
-const numbers = [65, 44, 12, 4];
-numbers.forEach(myFunction);
-
-function myFunction(item) {
-  sum += item;
-}
 
 // HimanshuPatni
