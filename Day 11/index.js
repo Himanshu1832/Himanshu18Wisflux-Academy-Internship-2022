8406f6c73f7302c25a30df5ed17f55d7c8fd2433
Himanshu Patni <96659387+Himanshu1832@users.noreply.github.com>
2022-07-18 21:12:34 +0530

    Update index.js

--- Day 11/index.js
+++ Day 11/index.js
@@ -17,20 +17,33 @@ form.addEventListener("submit", function (e) {
         .then((result) => result.json())
         .then((data) => {
           var result = document.getElementById("result");
+
           for (i = 0; i < 9; i++) {
+
             let li = document.createElement("h1");
             let a = document.createElement("a");
+            // a=data[i].repo.url;
             var q = "https://www.github.com/" + original;
             a.href = q;
             a.innerText = "click";
+            a.className = "ahref";
+            li.className = "h1";
             console.log(a);
             li.innerText = data[i].repo.name;
             result.appendChild(a);
             result.appendChild(li);
+            
           }
         });
     });
 });
 
+let sum = 0;
+const numbers = [65, 44, 12, 4];
+numbers.forEach(myFunction);
 
-// HimanshuPatni
+function myFunction(item) {
+  sum += item;
+}
+// <img src = "${data.events_url}"/>
+// main
